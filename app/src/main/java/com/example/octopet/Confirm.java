dc2a713064f03f6e266a3302a91ae987c66c07af
arthurtham <19481191+arthurtham@users.noreply.github.com>
2019-04-13 09:24:45 -0700

    Good/bad implementated

--- app/src/main/java/com/example/octopet/Confirm.java
+++ app/src/main/java/com/example/octopet/Confirm.java
@@ -8,6 +8,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.util.IOUtils;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Confirm extends MainActivity {
 
     Button option1;
@@ -30,6 +38,10 @@ public class Confirm extends MainActivity {
         getWindow().setLayout((int) (1 * width), (int) (1 * height));
 
         final String food;
+        option1 = (Button)findViewById(R.id.button1);
+        option2 = (Button)findViewById(R.id.button2);
+        option3 = (Button)findViewById(R.id.button3);
+
         Bundle extra = getIntent().getExtras();
         if (extra == null) {
             food = "whatever that is";
@@ -37,13 +49,13 @@ public class Confirm extends MainActivity {
         else {
             food = "yes!";
             try {
-                option1 = (Button)findViewById(R.id.button1);
+
                 option1.setText(extra.getString("option1"));
                 System.out.println(extra.getString("option1"));
-                option2 = (Button)findViewById(R.id.button2);
+
                 option2.setText(extra.getString("option2"));
                 System.out.println(extra.getString("option2"));
-                option3 = (Button)findViewById(R.id.button3);
+
                 option3.setText(extra.getString("option3"));
                 System.out.println(extra.getString("option3"));
             } catch (Exception e){
@@ -72,21 +84,33 @@ public class Confirm extends MainActivity {
 
         option1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                if (patternIsGood((String) option1.getText())) {
+                    System.out.println("Good choice!");
+                } else {
+                    System.out.println("I don't feel so good chief");
+                }
                 startActivity(new Intent(Confirm.this, MainActivity.class));
             }
         });
 
         option2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                if (patternIsGood((String) option2.getText())) {
+                    System.out.println("Good choice!");
+                } else {
+                    System.out.println("I don't feel so good chief");
+                }
                 startActivity(new Intent(Confirm.this, MainActivity.class));
             }
         });
 
         option3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                if (patternIsGood((String) option3.getText())) {
+                    System.out.println("Good choice!");
+                } else {
+                    System.out.println("I don't feel so good chief");
+                }
                 startActivity(new Intent(Confirm.this, MainActivity.class));
             }
         });
@@ -98,6 +122,46 @@ public class Confirm extends MainActivity {
             }
         });
 
+    }
+
+    private String openFile(String fileName) {
+        try {
+            InputStream is = getApplicationContext().getAssets().open(fileName);
+            Scanner s = new Scanner(is).useDelimiter("\\A");
+            String result = s.hasNext() ? s.next() : "";
+            result = result.replaceAll("[^A-Za-z0-9]", " ");
+            //System.out.println(result);
+            return result;
+        } catch (Exception e) {
+            //File load error
+            return "error";
+        }
+    }
+
+    private Boolean stringMatch(String patternStr, String fileStr) {
+        String[] patternArray = patternStr.split("[^A-Za-z0-9]");
+        for (String patternToken : patternArray) {
+            System.out.println("Pattern:" + patternToken.toLowerCase());
+            System.out.println("fileStr:" + fileStr);
+            if (fileStr.contains(patternToken.toLowerCase())) {
+                System.out.println("The pattern matched!");
+                return true;
+            }
+        }
+        System.out.println("No patterns matched.");
+        return false;
+    }
+
+    private Boolean patternIsGood(String pattern) {
+        System.out.println("Checking bad list...");
+        String badList = openFile("bad.txt");
+        if (stringMatch(pattern,badList)) {
+            System.out.println("This item is in the bad list.");
+            return false; // it's a bad item
+        }
+        System.out.println("Checking good list...");
+        String goodList = openFile("good.txt");
+        return (stringMatch(pattern,goodList)); //true: it's a good item. false: it's a bad item
     }
 
 }
